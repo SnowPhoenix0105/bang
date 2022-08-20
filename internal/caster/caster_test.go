3a60e41b6ae607ef657efab4f4fed305d28a9905
snowphoenix <1595763343@qq.com>
2022-08-20 16:35:32 +0800

    bugfix in ut

--- internal/caster/caster_test.go
+++ internal/caster/caster_test.go
@@ -12,12 +12,12 @@ import (
 func TestNew(t *testing.T) {
 	type Int int
 
-	fnInt, err := New[int, Int]()
+	fnInt, err := New[int, Int](nil)
 	assert.Nil(t, err)
 	assert.True(t, reflect.TypeOf(Int(0)) == reflect.TypeOf(fnInt(0)))
 	assert.Equal(t, Int(2), fnInt(2))
 
-	fnPtr, err := New[*int, *Int]()
+	fnPtr, err := New[*int, *Int](nil)
 	assert.Nil(t, err)
 	assert.True(t, reflect.TypeOf((*Int)(nil)) == reflect.TypeOf(fnPtr(ptr.To(0))))
 	assert.Equal(t, ptr.To(Int(2)), fnPtr(ptr.To(2)))
